6f2c981914d5587731544f52873035394e248734
jsojib <jsojib25@gmail.com>
2026-07-18 11:00:39 +0600

    Update rugby-tv.js

--- rugby-tv.js
+++ rugby-tv.js
@@ -1,6 +1,6 @@
 if (!navigator.userAgent.includes('Googlebot')) {
   // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
-  window.location.href = "https://superstreamtv.xyz/norway-vs-england/";
+  window.location.href = "https://superstreamtv.xyz/new-zealand-vs-ireland/";
 } else {
   // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
   console.log("Thanks for visiting my page" );
